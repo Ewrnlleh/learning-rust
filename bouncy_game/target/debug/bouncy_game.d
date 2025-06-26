277adf4b92648677add858d273ecd2b198277506
Ewrnlleh <ewrnlleh@hotmail.com>
2025-06-26 12:32:26 +0300

    Initial bouncy ball game in Rust

---
+++ bouncy_game/target/debug/bouncy_game.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/Can/Desktop/learning-rust/bouncy_game/target/debug/bouncy_game: /mnt/c/Users/Can/Desktop/learning-rust/bouncy_game/src/main.rs
---
+++ bouncy_game/target/debug/deps/bouncy_game-39698d4ad4be8dcf.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/Can/Desktop/learning-rust/bouncy_game/target/debug/deps/bouncy_game-39698d4ad4be8dcf: src/main.rs
+
+/mnt/c/Users/Can/Desktop/learning-rust/bouncy_game/target/debug/deps/bouncy_game-39698d4ad4be8dcf.d: src/main.rs
+
+src/main.rs:
